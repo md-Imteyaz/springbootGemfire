6ec51293550b8912e3c3ffa5db18cb0a2ab23ae2
md-Imteyaz <98022042+md-Imteyaz@users.noreply.github.com>
2022-07-15 12:05:10 +0530

    Add files via upload

---
+++ SpringdataGemFireApplication.java
@@ -0,0 +1,13 @@
+package com.SpringbootAGemFire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataGemFireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataGemFireApplication.class, args);
+	}
+
+}
